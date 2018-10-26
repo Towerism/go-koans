f863765207cb38db488fd33b4fde6e81258f3011
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-10-25 22:28:28 -0500

    Fix concurrency tests

--- about_concurrency.go
+++ about_concurrency.go
@@ -10,19 +10,19 @@ func isPrimeNumber(possiblePrime int) bool {
 }
 
 func findPrimeNumbers(channel chan int) {
-	for i := 2; ; /* infinite loop */ i++ {
-		// your code goes here
-
-		assert(i < 100) // i is afraid of heights
+	for i := 2; i < 12; i++ {
+		if isPrimeNumber(i) {
+			channel <- i
+		}
 	}
 }
 
 func aboutConcurrency() {
 	ch := make(chan int)
 
-	assert(__delete_me__) // concurrency can be almost trivial
-	// your code goes here
-
+	go func() {
+		findPrimeNumbers(ch)
+	}()
 	assert(<-ch == 2)
 	assert(<-ch == 3)
 	assert(<-ch == 5)
